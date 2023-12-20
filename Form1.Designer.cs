76da02cf7d72d25de862f4e89a204c8ba159fbf1
ADMIN <ADMIN@DESKTOP-CBRV7QC>
2023-12-20 21:27:52 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgMain = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.времяОбновленияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMain)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgMain
+            // 
+            this.dgMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgMain.ContextMenuStrip = this.contextMenuStrip1;
+            this.dgMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgMain.Location = new System.Drawing.Point(0, 0);
+            this.dgMain.Name = "dgMain";
+            this.dgMain.Size = new System.Drawing.Size(800, 450);
+            this.dgMain.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.времяОбновленияToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(180, 26);
+            // 
+            // времяОбновленияToolStripMenuItem
+            // 
+            this.времяОбновленияToolStripMenuItem.Name = "времяОбновленияToolStripMenuItem";
+            this.времяОбновленияToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.времяОбновленияToolStripMenuItem.Text = "Время обновления";
+            this.времяОбновленияToolStripMenuItem.Click += new System.EventHandler(this.времяОбновленияToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgMain)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgMain;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem времяОбновленияToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace TaskManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DataColumn idColumn = new DataColumn("Id", Type.GetType("System.Int32"));
+            DataColumn nameColumn = new DataColumn("Name", Type.GetType("System.String"));
+            DataColumn timeStart = new DataColumn("TimeStart", Type.GetType("System.String"));
+            DataColumn timeProcessor = new DataColumn("TimeProcessor", Type.GetType("System.String"));
+            DataColumn countTreads = new DataColumn("CountTreads", Type.GetType("System.Int32"));
+
+            dt.Columns.Add(idColumn);
+            dt.Columns.Add(nameColumn);
+            dt.Columns.Add(timeStart);
+            dt.Columns.Add(timeProcessor);
+            dt.Columns.Add(countTreads);
+            //dgMainFill(dgMain);
+            reloadTime = 1000;
+            timer1 = new System.Threading.Timer(dgMainFill, dgMain, 50, reloadTime);
+
+        }
+
+        private static DataTable dt = new DataTable();
+        private static Process[] processes;
+        public static Int32 reloadTime;
+        System.Threading.Timer timer1;
+
+        
+        public void dgMainFill(object sender)
+        {
+            processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                string s, t;
+                //ДОБАВИТЬ В МЕНЮСТРИП
+                //process.Kill(); // принудительное закрытие
+                //process.CloseMainWindow(); // закрывает с диалогом
+                try 
+                { 
+                    s = process.StartTime.ToString(); 
+                } catch { s = ""; }
+                try 
+                { 
+                    t = process.TotalProcessorTime.ToString(); 
+                } catch { t = ""; }
+                try 
+                { 
+                    dt.Rows.Add(new object[] { process.Id, process.ProcessName, s, t, process.Threads.Count }); 
+                } catch { }
+            }
+            //try
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    (sender as DataGridView).DataSource = dt;
+                });
+            }
+            //catch { }
+           
+        }
+        // привязать контекстное меню к гриду 
+        private void времяОбновленияToolStripMenuItem_Click(object sender, EventArgs e)
+        {            
+            Form form = new Form2();
+            form.ShowDialog();
+            timer1.Change(reloadTime, reloadTime);
+        }
+    }
+}
